52dde69ff56307021920788a3528abcc015afa44
magiran <andreycu@mail.ru>
2021-04-07 16:37:12 +0300

    Added OrderUpdate controller with using Django CBV

--- ordersapp/views.py
+++ ordersapp/views.py
@@ -3,7 +3,7 @@ from django.forms import inlineformset_factory
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy, reverse
-from django.views.generic import ListView, CreateView, DeleteView, DetailView
+from django.views.generic import ListView, CreateView, DeleteView, DetailView, UpdateView
 
 from basketapp.models import Basket
 from .models import Order, OrderItem
@@ -90,5 +90,41 @@ def order_forming_complete(request, pk):
 
 
 
-class OrderItemsUpdate(ListView):
-    pass
+
+
+
+class OrderUpdate(LoginRequiredMixin, UpdateView):
+    model = Order
+    fields = []
+    context_object_name = 'object'
+    success_url = reverse_lazy('ordersapp:orders_list')
+
+    def get_context_data(self, **kwargs):
+        data = super(OrderUpdate, self).get_context_data(**kwargs)
+        OrderFormSet = inlineformset_factory(Order, OrderItem, form=OrderItemsForm, extra=1)
+
+        if self.request.POST:
+            formset = OrderFormSet(self.request.POST, instance=self.object)
+        else:
+            formset = OrderFormSet(instance=self.object)
+
+        data['orderitems'] = formset
+
+        return data
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        orderitems = context['orderitems']
+
+        with transaction.atomic():
+            form.instance.user = self.request.user
+            self.object = form.save()
+            if orderitems.is_valid():
+                orderitems.instance = self.object
+                orderitems.save()
+
+            # удаляем пустой заказ
+            if self.object.get_total_cost() == 0:
+                self.object.delete()
+
+            return super(OrderUpdate, self).form_valid(form)
