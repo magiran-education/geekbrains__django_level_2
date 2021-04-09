62ea7b799363eddc9ba34bb7a9e099616b86f393
AndreySK <andreycu@mail.ru>
2021-04-09 04:40:31 +0300

    example without django-dinamic-formset

--- ordersapp/models.py
+++ ordersapp/models.py
@@ -69,3 +69,8 @@ class OrderItem(models.Model):
 
     def get_product_cost(self):
         return self.product.price * self.quantity
+
+    def delete(self):
+        self.product.quantity += self.quantity
+        self.product.save()
+        super(OrderItem, self)
